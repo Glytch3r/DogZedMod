d78992229371cd335c7450d462f2e58475c7c08e
Glytch3r <glytch3r.arts@gmail.com>
2024-11-04 05:41:50 +0800

    added fence hop for dog player, added sound effects for dog zeds

---
+++ .history/media/lua/client/DogZedMod_SFX_20241104033620.lua
@@ -0,0 +1,218 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       		 Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							  |
+|                       		 Discord:    Glytch3r#1337 / glytch3r															  |
+|                       		 Support:    https://ko-fi.com/glytch3r														 	  |
+|_________________________________________________________________________________________________________________________________|
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████-]]
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+DogZedMod = DogZedMod or {}
+
+DogZedMod.sfxZedList = {
+	'DogZed_Howl_1','DogZed_Howl_2',
+	'DogZed_Bark_1', 'DogZed_Bark_2', 'DogZed_Bark_3',
+	'DogZed_Bark_4', 'DogZed_Bark_5', 'DogZed_Bark_6',
+	'DogZed_Pant_1', 'DogZed_Pant_2',
+	--'DogZed_Whimper_1', 'DogZed_Whimper_2'
+}
+
+function DogZedMod.VolumeHandler(dist, range)
+    if dist >= range then
+        return 0
+    elseif dist <= 0 then
+        return 1
+    else
+        return 1 - (dist / range)
+    end
+end
+
+function DogZedMod.getBarkSfx()
+	return 'DogZed_Bark_'..tostring(ZombRand(1,7))
+end
+function DogZedMod.getHowlSfx()
+	return 'DogZed_Howl_'..tostring(ZombRand(1,3))
+end
+function DogZedMod.getWhimperSfx()
+	return 'DogZed_Whimper_'..tostring(ZombRand(1,3))
+end
+function DogZedMod.getPantSfx()
+	return 'DogZed_Pant_'..tostring(ZombRand(1,3))
+end
+
+
+function DogZedMod.isOnAlert(zed)
+	if not DogZedMod.isDogZed(zed) then return end
+	return zed:isCurrentState(ZombieTurnAlerted.instance())
+end
+
+
+
+function DogZedMod.doHowl(zed)
+	if not DogZedMod.isDogZed(zed) then return end
+	local cd = zed:getModData()['HowlCD']
+	if not cd then
+		DogZedMod.addCooldown(zed, 3)
+		DogZedMod.playSFX(zed, DogZedMod.getHowlSfx())
+	end
+end
+
+
+
+function DogZedMod.addCooldown(zed, count)
+	zed:getModData()['HowlCD'] = true
+	count = count or 3
+	local function HowlCDHandler()
+		count = count - 1
+		local cd = zed:getModData()['HowlCD']
+		if count <= 0 or not cd  then
+			zed:getModData()['HowlCD'] = nil
+			Events.EveryHours.Remove(HowlCDHandler)
+		end
+	end
+
+	Events.EveryHours.Add(HowlCDHandler)
+end
+
+function DogZedMod.playSFX(zed, sfx)
+	if not DogZedMod.isDogZed(zed) then return end
+	local range = 50
+	local pl = getPlayer(); if not pl then return end
+	if DogZedMod.isClosestPl(pl, zed) or pl == zed then
+		local sq = zed:getSquare()
+		if sq then
+			local vol = DogZedMod.VolumeHandler(DogZedMod.checkDist(pl, zed), range)
+			if luautils.stringStarts(sfx, "DogZed_Howl") or string.find(sfx, "DogZed_Howl")  then
+				if SandboxVars.DogZedMod.HowlAttracts then
+					addSound(zed, zed:getX(), zed:getY(), zed:getZ(), 50, vol);
+				end
+			end
+			getSoundManager():PlayWorldSound(tostring(sfx), sq, 0, 50, vol, false);
+		end
+	end
+end
+
+
+
+DogZedMod.sfxBanList = {
+	--'ZombieSurprisedPlayer', 'BodyHitGround',
+	'ZombieThumpMetal', 'ZombieThumpGeneric', 'BurningFlesh', 'MaleZombieCombined', 'FemaleZombieCombined',
+	'ZombieFootstepsCombined', 	'ZombieThumpVehicle', 'ZombieThumpVehicleWindow', 'ZombieThumpWindow',
+	'ZombieThumpBarbedFence', 'ZombieThumpGarageDoor', 'ZombieScratch', 'ZombieBite', 'ZombieCrawlLungeSwing',
+	'ZombieCrawlLungeHit',  'ZombieTrip', 'TutorialZombie', 'MaleZombieIdle', 'FemaleZombieIdle',
+	'MaleZombieHurt', 'FemaleZombieHurt', 'FemaleZombieAttack', 'MaleZombieDeath', 'FemaleZombieDeath',
+	'FemaleBeingEatenDeath', 'FemaleHeavyBreath', 'fzombiedeath', 'MaleHeavyBreathPanic', 'MaleHeavyBreath',
+	'FemaleHeavyBreathPanic', 'MaleBeingEatenDeath', 'ZombieFootstepGrass', 'ZombieFootstepWood', 'ZombieFootstepConcrete',
+	'fzombierand', 'fzombierand1', 'fzombierand2', 'fzombierand3', 'fzombierand4', 'fzombierand5', 'fzombierand6',
+	'fzombierand7', 'fzombierand8', 'fzombierand9', 'fzombiehit', 'fzombiehit1', 'fzombiehit2', 'fzombiehit3',
+	'fzombiehit4', 'fzombiehit5', 'PZ_FemaleBeingEaten_Death', 	'PZ_FemaleZombieEating', 'PZ_Female_zombie',
+	'PZ_Female_zombie_01', 'PZ_Female_zombie_02', 'PZ_Female_zombie_03', 'PZ_Female_zombie_04', 'PZ_Female_zombie_05',
+	'PZ_Female_zombie_06', 'PZ_Female_zombie_07', 'PZ_Female_zombie_08', 'PZ_Female_zombie_09', 'PZ_Female_zombie_Death',
+	'PZ_Female_zombie_Death_01', 'PZ_Female_zombie_Death_02', 'PZ_Female_zombie_Death_03', 'PZ_Female_zombie_Death_04',
+	'PZ_MaleBeingEaten_Death', 'PZ_MaleDeath', 'PZ_MaleDeath_01', 'PZ_MaleDeath_02', 'PZ_MaleDeath_03', 'PZ_MaleDeath_04',
+	'PZ_MaleDeath_05', 'PZ_MaleDeath_06', 'PZ_MaleDeath_07', 'PZ_MaleZombieEating', 'PZ_MaleZombie', 'PZ_MaleZombie_01',
+	'PZ_MaleZombie_02', 'PZ_MaleZombie_03', 'PZ_MaleZombie_04', 'PZ_MaleZombie_05', 'PZ_MaleZombie_06', 'PZ_MaleZombie_07',
+	'PZ_MaleZombie_08', 'zombieambient1', 'zombiebite',	'zombieeating', 'zombieImpact', 'zombierand', 'zombierand1',
+	'zombierand2', 'zombierand3', 'zombierand4', 'zombierand5', 'zombierand6', 'zombierand7', 'zombierand8', 'zombierand9',
+	'zombie_footstep_m', 'zombie_footstep_m_01', 'zombie_footstep_m_02', 'zombie_footstep_m_03', 'zombie_footstep_m_04',
+	'zombie_footstep_m_05', 'zombie_footstep_m_06',	'zombie_footstep_m_07', 'zombie_footstep_m_08', 'zombie_footstep_m_09',
+	'zombie_footstep_m_10', 'zombie_footstep_m_11', 'zombie_footstep_m_12',
+	'zombie_vocal_C_aggro','zombie_vocal_B_aggro', 'zombie_vocal_A_aggro',
+	'Zombie_Female_A_Aggro','Zombie_Female_B_Aggro','Zombie_Female_C_Aggro', 'zombie_vocal',
+	'zombie_male_vocal_C_aggro','zombie_male_vocal_B_aggro', 'zombie_male_vocal_A_aggro',
+}
+
+function DogZedMod.getSFX()
+	return DogZedMod.sfxZedList[ZombRand(1, #list)]
+end
+
+function DogZedMod.preSFX(zed)
+	if not DogZedMod.isDogZed(zed) then return end
+	if zed:getEmitter() then
+		local audioName = nil
+		local list = DogZedMod.sfxBanList
+		for _, audioName in ipairs(list) do
+			if zed:getEmitter():isPlaying(audioName) then
+				zed:getEmitter():stopSoundByName(tostring(audioName))
+				getWorld():getFreeEmitter():stopSoundByName(tostring(audioName))
+				local sfxRate = SandboxVars.DogZedMod.SFXRate or 5
+				if DogZedMod.doRoll(sfxRate) then
+					DogZedMod.playRandSfx(zed)
+				end
+			end
+		end
+	end
+end
+Events.OnZombieUpdate.Remove(DogZedMod.preSFX)
+Events.OnZombieUpdate.Add(DogZedMod.preSFX)
+
+
+
+function DogZedMod.playRandSfx(zed)
+	if not DogZedMod.isDogZed(zed) then return end
+	if DogZedMod.doRoll(5) then
+		local toPlay = DogZedMod.getSFX()
+		zed:getEmitter():playSound(toPlay)
+	end
+end
+
+
+
+
+function DogZedMod.keypress(key)
+	local pl = getPlayer()
+
+    if key == getCore():getKey("Shout") then
+		if DogZedMod.isDogPl(pl)   then
+			local sfx = nil
+			if pl:isSneaking() then
+				sfx = DogZedMod.getPantSfx()
+				DogZedMod.playPlSfx(pl, sfx, false)
+			elseif  pl:IsAiming()  then
+				sfx = DogZedMod.getHowlSfx()
+				DogZedMod.playPlSfx(pl, sfx, true)
+				if DogZedMod.getAdminSkinNum(pl) == 0 then
+					DogZedMod.addHL(pl)
+					DogZedMod.setRadiateSq(pl)
+				end
+			else
+				sfx = DogZedMod.getBarkSfx()
+				DogZedMod.playPlSfx(pl, sfx, false)
+				if DogZedMod.getAdminSkinNum(pl) == 0 then
+					DogZedMod.setRadiateArea(pl)
+				end
+			end
+		end
+
+    end
+	return key
+end
+Events.OnKeyPressed.Remove(DogZedMod.keypress)
+Events.OnKeyPressed.Add(DogZedMod.keypress)
+
+
+function DogZedMod.OnZombieDead(zed)
+	if not DogZedMod.isDogZed(zed) then return end
+	DogZedMod.playSFX(zed, DogZedMod.getWhimperSfx())
+end
+Events.OnZombieDead.Remove(DogZedMod.OnZombieDead)
+Events.OnZombieDead.Add(DogZedMod.OnZombieDead)
